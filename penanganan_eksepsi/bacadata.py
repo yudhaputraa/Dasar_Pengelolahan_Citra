d63236b421630a36589ce91ea67330ff07dfcb26
yudhaputra <elekronikayudha@gmail.com>
2023-02-24 13:27:21 +0700

    pembacaan data berkas teks mengunakan pernyataan try..finally

---
+++ penanganan_eksepsi/bacadata.py
@@ -0,0 +1,17 @@
+# pembacaan data berkas teks mengunakan pernyataan try..finally
+
+import sys
+
+if len(sys.argv) == 1:
+    print('masukkan nama berkas yang ingin dibaca')
+else:
+    berkas = sys.argv[1]
+
+    try:
+        pegangan = open(berkas, 'r')
+        konten = pegangan.read()
+        print('isi berkas:')
+        print(konten)
+    finally:
+        print('Operasi pembacaan berkas telah berakhir')
+    print('*****')
