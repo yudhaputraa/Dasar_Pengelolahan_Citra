fe33024ffe21990b9d9de1fb5d609b8405c603f3
yudhaputra <elekronikayudha@gmail.com>
2023-03-26 12:58:58 +0700

    pembuatan garis tegak dan mendaftar memalui titik tengah citra

---
+++ Dasar_pengolahan_citra/garispusat.py
@@ -0,0 +1,28 @@
+# pembuatan garis tegak dan mendaftar memalui titik tengah citra
+
+import cv2
+import sys
+
+if len(sys.argv) == 1:
+    print('Masukan nama berkas gambar')
+else:
+    berkas = sys.argv[1]
+    citra = cv2.imread(berkas, cv2.IMREADUNCHANGED)
+    if citra is None:
+        print('Tidak dpt membaca berkas', berkas)
+    else:
+        jumBaris = citra.shape[0]
+        jumKolom = citra.shape[1]
+        xTengah = jumKolom // 2
+        yTengah = jumBaris // 2
+        
+        # Buat garis tegak
+        cv2.line(citra, (xTengah, 0), (yTengah, jumBaris - 1), [128,128,128], 5)
+
+        # Buat garis mendatar
+        cv2.line(citra, (0, yTengah), (jumKolom - 1, yTengah), [128,128,128], 5)
+
+        # Tampilkan citra
+        cv2.imshow('hasil', citra)
+
+        cv2.waitKey(0)
