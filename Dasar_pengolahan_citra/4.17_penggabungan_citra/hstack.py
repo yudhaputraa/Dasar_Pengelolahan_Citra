d70e3b90f38e9a88f5682e825f67d0ea2a571ea3
yudhaputra <elekronikayudha@gmail.com>
2023-08-29 06:56:20 +0700

    penggabungan dua gambar secara bersebelahan

---
+++ Dasar_pengolahan_citra/4.17_penggabungan_citra/hstack.py
@@ -0,0 +1,16 @@
+# penggabungan dua gambar secara bersebelahan
+
+import cv2
+import numpy as np
+
+goldhill = cv2.imread('goldhill2.jpg')
+citraA =  cv2.resize(goldhill, (512, 512))
+baboon = cv2.imread('Baboon.png')
+citraB =  cv2.resize(baboon, (512, 512))
+
+hasil = np.hstack((citraA,citraB))
+
+# Tampilkan hasilnya
+cv2.imshow('hasil', hasil)
+
+cv2.waitKey(0)
