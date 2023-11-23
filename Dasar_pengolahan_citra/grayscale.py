416db8edf997fe9a3fc0b05853f3b7e2b7069122
yudhaputra <elekronikayudha@gmail.com>
2023-11-23 13:01:31 +0700

    Pembacaan baboon.png dalam bentuk skala keabu-abuan

---
+++ Dasar_pengolahan_citra/grayscale.py
@@ -0,0 +1,10 @@
+# Pembacaan baboon.png dalam bentuk skala keabu-abuan
+
+import cv2
+
+# IMREAD_GRAYSCALE() membuat gambar menjadi abu-abu
+citra = cv2.imread('Baboon.png', cv2.IMREAD_GRAYSCALE)
+if not citra is None:
+    cv2.imshow('Gambar Baboon.png', citra)
+    cv2.waitKey(0)
+    
